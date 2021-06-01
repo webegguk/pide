d7f7437ac467216271de8a0aecf54068c174e0ad
Webegg <matt3gg@hotmail.com>
2021-06-01 17:27:03 +0100

    update to startup

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "jose"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 # app.config["JWT_AUTH_URL_RULE"] = "/login"
 # app.config["JWT_EXPIRATION_DELTA"] = timedelta(seconds=1800)
@@ -53,5 +50,4 @@ api.add_resource(UserRegister, "/register")
 api.add_resource(StoreList, "/stores")
 
 if __name__ == "__main__":
-    db.init_app(app)
     app.run(port=5000, debug=True)
